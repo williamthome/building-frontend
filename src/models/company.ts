7a1fc90955d251fab1c0e3e28bb501f047071497
William Fank Thomé <willegp88@gmail.com>
2021-01-08 13:49:46 -0300

    feat: create company model

---
+++ src/models/company.ts
@@ -0,0 +1,29 @@
+export interface Company {
+  id: string
+  name: string
+  members: Member[]
+}
+
+export interface Member {
+  userId: string
+  companyRole: CompanyRole
+  features: UserFeatures
+}
+
+export enum CompanyRole {
+  owner = 0,
+  master = 1,
+  user = 2
+}
+
+export enum UserFeatures {
+  None = 0,
+  ManageBuildings = 1 << 0,
+  ManageProjects = 1 << 1,
+  ManageUsers = 1 << 2,
+  ManageCompanyData = 1 << 3,
+  ManagePhases = 1 << 4,
+  ManageCustomers = 1 << 5,
+  ManageProperties = 1 << 6,
+  ManageTechnicians = 1 << 7
+}
